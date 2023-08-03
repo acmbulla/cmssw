f1ab5b2694b89d79def5046a189133d2fab5e1d5
acmbulla <a.bulla@campus.unimib.it>
2023-08-03 18:32:25 +0200

    eff histo vs lumis

--- DQM/TrackingMonitorSource/src/TrackToTrackComparisonHists.cc
+++ DQM/TrackingMonitorSource/src/TrackToTrackComparisonHists.cc
@@ -6,6 +6,7 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/BeamSpot/interface/BeamSpot.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/Luminosity/interface/LumiSummary.h"
 
 #include "DataFormats/Math/interface/deltaR.h"
 #include "CommonTools/TriggerUtils/interface/GenericTriggerEventFlag.h"
@@ -35,6 +36,8 @@ TrackToTrackComparisonHists::TrackToTrackComparisonHists(const edm::ParameterSet
   referenceBSToken_ = consumes<reco::BeamSpot>(iConfig.getParameter<edm::InputTag>("referenceBeamSpot"));
   monitoredPVToken_ = consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("monitoredPrimaryVertices"));
   referencePVToken_ = consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("referencePrimaryVertices"));
+  lumiScalersToken_ = consumes<LumiScalersCollection>(iConfig.getParameter<edm::InputTag>("scalers"));
+  onlineMetaDataDigisToken_ = consumes<OnlineLuminosityRecord>(iConfig.getParameter<edm::InputTag>("onlineMetaDataDigis"));
 
   referenceTracksMEs_.label = referenceTrackInputTag_.label();
   matchedReferenceTracksMEs_.label = referenceTrackInputTag_.label() + "_matched";
@@ -240,6 +243,48 @@ void TrackToTrackComparisonHists::analyze(const edm::Event& iEvent, const edm::E
       << "Total matched reference tracks: " << nMatchedReferenceTracks << "\n"
       << "Total monitored tracks: " << nMonitoredTracks << "\n"
       << "Total unMatched monitored tracks: " << nUnmatchedMonitoredTracks << "\n";
+
+
+  //
+  //  Get Lumi/LS Info and filling (pre booked) histos
+  //
+
+  unsigned int const ls = iEvent.id().luminosityBlock();
+
+  double onlinelumi = -1.f;
+  double PU = -1.f;
+
+  auto const lumiScalersHandle = iEvent.getHandle(lumiScalersToken_);
+  auto const onlineMetaDataDigisHandle = iEvent.getHandle(onlineMetaDataDigisToken_);
+
+  if (onlineMetaDataDigisHandle.isValid()) {
+    onlinelumi = onlineMetaDataDigisHandle->instLumi();
+    PU = onlineMetaDataDigisHandle->avgPileUp();
+  } else if ( lumiScalersHandle.isValid() and not lumiScalersHandle->empty() ){
+    auto const scalit = lumiScalersHandle->begin();
+    onlinelumi = scalit->instantLumi();
+    PU = scalit->pileup();
+  }
+
+  (referenceTracksMEs_.h_nTracks_onlinelumi)->Fill(onlinelumi, nReferenceTracks);
+  (referenceTracksMEs_.h_nTracks_ls)->Fill(ls, nReferenceTracks);
+  (referenceTracksMEs_.h_nTracks_PU)->Fill(PU, nReferenceTracks);
+
+  (matchedReferenceTracksMEs_.h_nTracks_onlinelumi)->Fill(onlinelumi, nMatchedReferenceTracks);
+  (matchedReferenceTracksMEs_.h_nTracks_ls)->Fill(ls, nMatchedReferenceTracks);
+  (matchedReferenceTracksMEs_.h_nTracks_PU)->Fill(PU, nMatchedReferenceTracks);
+
+  (monitoredTracksMEs_.h_nTracks_onlinelumi)->Fill(onlinelumi, nMonitoredTracks);
+  (monitoredTracksMEs_.h_nTracks_ls)->Fill(ls, nMonitoredTracks);
+  (monitoredTracksMEs_.h_nTracks_PU)->Fill(PU, nMonitoredTracks);
+
+  (unMatchedMonitoredTracksMEs_.h_nTracks_onlinelumi)->Fill(onlinelumi, nUnmatchedMonitoredTracks);
+  (unMatchedMonitoredTracksMEs_.h_nTracks_ls)->Fill(ls, nUnmatchedMonitoredTracks);
+  (unMatchedMonitoredTracksMEs_.h_nTracks_PU)->Fill(PU, nUnmatchedMonitoredTracks);
+
+  // what about book_matching_tracks_histos(ibooker, matchTracksMEs_, "matches", dir);??? devo riempire anche questo?
+  // in this case whatch out for the header of MatchingME!
+
 }
 
 void TrackToTrackComparisonHists::bookHistograms(DQMStore::IBooker& ibooker,
@@ -267,6 +312,7 @@ void TrackToTrackComparisonHists::bookHistograms(DQMStore::IBooker& ibooker,
   bookHistos(ibooker, unMatchedMonitoredTracksMEs_, "mon_unMatched", dir);
 
   book_matching_tracks_histos(ibooker, matchTracksMEs_, "matches", dir);
+
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
@@ -282,6 +328,8 @@ void TrackToTrackComparisonHists::fillDescriptions(edm::ConfigurationDescription
   desc.add<edm::InputTag>("referenceTrack", edm::InputTag("generalTracks"));
   desc.add<edm::InputTag>("referenceBeamSpot", edm::InputTag("offlineBeamSpot"));
   desc.add<edm::InputTag>("referencePrimaryVertices", edm::InputTag("offlinePrimaryVertices"));
+  desc.add<edm::InputTag>("scalers", edm::InputTag("hltScalersRawToDigi"));
+  desc.add<edm::InputTag>("onlineMetaDataDigis", edm::InputTag("hltOnlineMetaDataDigis"));
 
   desc.add<std::string>("topDirName", "HLT/Tracking/ValidationWRTOffline");
   desc.add<double>("dRmin", 0.002);
@@ -297,6 +345,7 @@ void TrackToTrackComparisonHists::fillDescriptions(edm::ConfigurationDescription
   edm::ParameterSetDescription histoPSet;
   fillHistoPSetDescription(histoPSet);
   desc.add<edm::ParameterSetDescription>("histoPSet", histoPSet);
+
 
   descriptions.add("trackToTrackComparisonHists", desc);
 }
@@ -380,6 +429,14 @@ void TrackToTrackComparisonHists::book_generic_tracks_histos(DQMStore::IBooker& 
                                      Pt_nbin,
                                      Pt_rangeMin,
                                      Pt_rangeMax);
+                        
+  // counts of tracks vs lumi
+  // for this moment, xmin,xmax and binning are hardcoded, maybe in future in a config file!
+  // have to add (declare) this in the .h file as well
+  (mes.h_nTracks_onlinelumi) = ibooker.book1D(label + "nTracks_vs_onlinelumi", "number of tracks vs onlinelumi", onlinelumi_nbin, onlinelumi_rangeMin, onlinelumi_rangeMax);
+  (mes.h_nTracks_ls) = ibooker.book1D(label + "nTracks_vs_ls", "number of tracks vs ls", ls_nbin, ls_rangeMin, ls_rangeMax);
+  (mes.h_nTracks_PU) = ibooker.book1D(label + "nTracks_vs_PU", "number of tracks vs PU", PU_nbin, PU_rangeMin, PU_rangeMax);
+
 }
 
 void TrackToTrackComparisonHists::book_matching_tracks_histos(DQMStore::IBooker& ibooker,
@@ -553,6 +610,18 @@ void TrackToTrackComparisonHists::initialize_parameter(const edm::ParameterSet& 
   dzRes_rangeMin = pset.getParameter<double>("dzRes_rangeMin");
   dzRes_rangeMax = pset.getParameter<double>("dzRes_rangeMax");
   dzRes_nbin = pset.getParameter<unsigned int>("dzRes_nbin");
+
+  ls_rangeMin = pset.getParameter<unsigned int>("ls_rangeMin");
+  ls_rangeMax = pset.getParameter<unsigned int>("ls_rangeMax");
+  ls_nbin = pset.getParameter<unsigned int>("ls_nbin");
+
+  onlinelumi_rangeMin = pset.getParameter<double>("onlinelumi_rangeMin");
+  onlinelumi_rangeMax = pset.getParameter<double>("onlinelumi_rangeMax");
+  onlinelumi_nbin = pset.getParameter<unsigned int>("onlinelumi_nbin");
+
+  PU_rangeMin = pset.getParameter<double>("PU_rangeMin");
+  PU_rangeMax = pset.getParameter<double>("PU_rangeMax");
+  PU_nbin = pset.getParameter<unsigned int>("PU_nbin");
 }
 
 void TrackToTrackComparisonHists::fillHistoPSetDescription(edm::ParameterSetDescription& pset) {
@@ -595,4 +664,16 @@ void TrackToTrackComparisonHists::fillHistoPSetDescription(edm::ParameterSetDesc
   pset.add<double>("dzRes_rangeMin", -0.05);
   pset.add<double>("dzRes_rangeMax", 0.05);
   pset.add<unsigned int>("dzRes_nbin", 150);
+
+  pset.add<unsigned int>("ls_rangeMin",0);
+  pset.add<unsigned int>("ls_rangeMax",3000);
+  pset.add<unsigned int>("ls_nbin",3000);
+
+  pset.add<double>("onlinelumi_rangeMin",1000.0);
+  pset.add<double>("onlinelumi_rangeMax",20000.0);
+  pset.add<unsigned int>("onlinelumi_nbin",100);
+
+  pset.add<double>("PU_rangeMin",0.0);
+  pset.add<double>("PU_rangeMax",200.0);
+  pset.add<unsigned int>("PU_nbin",200);
 }
